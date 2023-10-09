b646624e7108c35c184b788278f463672f1004cd
AdhamKhaledDev <adhamk109@outlook.com>
2023-10-08 17:57:45 -0700

    Program_In_main

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - The program's entry point
+ *
+ * Return: Always returns 0 to signify success
+ */
+int main(void)
+{
+    int digit;
+
+    for (digit = 0; digit < 10; digit++)
+    {
+        printf("%d\n", digit);
+    }
+
+    return (0);
+}
+
