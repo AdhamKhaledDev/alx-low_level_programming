b8db25fbe4804d506520e38e18fef4bf421b09c4
AdhamKhaledDev <adhamk109@outlook.com>
2023-10-08 17:42:37 -0700

    Print_lowercase_uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,22 @@
  */
 int main(void)
 {
-char lowercase = 'a';
-while (lowercase <= 'z')
-{
-putchar(lowercase);
-lowercase++;
-}
-char uppercase = 'A';
-while (uppercase <= 'Z')
-{
-putchar(uppercase);
-uppercase++;
-}
-putchar('\n');
-return (0);
-}
+    char lowercase = 'a';
+    char uppercase = 'A';
 
+    while (lowercase <= 'z')
+    {
+        putchar(lowercase);
+        lowercase++;
+    }
+
+    while (uppercase <= 'Z')
+    {
+        putchar(uppercase);
+        uppercase++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
