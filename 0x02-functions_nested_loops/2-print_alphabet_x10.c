9f389cb2bcd189e73beb2ae0b1b66069fdcfc9a1
AdhamKhaledDev <adhamk109@outlook.com>
2023-10-10 16:35:40 -0700

    A_To_Z

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 -  Prints the lowercase alphabet 10 times, followed by a new line
+ *
+ */
+void print_alphabet_x10(void)
+{
+int board, ch;
+for (board = 0; board <= 9; board++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
+}
