a6d2e5d9579acbca04074d85378c7832a3b10ab1
AdhamKhaledDev <adhamk109@outlook.com>
2023-10-10 16:14:13 -0700

    alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - A function to print the entire lowercase alphabet.
+ *
+ */
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
