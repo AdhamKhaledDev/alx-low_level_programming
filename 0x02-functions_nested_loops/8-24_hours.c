11a48442c22c45dee18bdb356776b480d12e084a
AdhamKhaledDev <adhamk109@outlook.com>
2023-10-10 18:40:43 -0700

    Minutes&Houres

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* jack_bauer - Display all the minutes in a day, following the
+*               Jack Bauer format, from 00:00 to 23:59.
+*/
+void jack_bauer(void)
+{
+unsigned int bauer;
+unsigned int jack;
+for (jack = 0; jack <= 23; jack++)
+{
+for (bauer = 0; bauer <= 59; bauer++)
+{
+_putchar((jack / 10) + 48);
+_putchar((jack % 10) + 48);
+_putchar(':');
+_putchar((bauer / 10) + 48);
+_putchar((bauer / 10) + 48);
+_putchar("\n");
+}
+}
+}
