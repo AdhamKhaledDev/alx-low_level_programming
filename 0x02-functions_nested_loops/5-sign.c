1cac34fbb0412752c595e7c2c11f063d2715575e
AdhamKhaledDev <adhamk109@outlook.com>
2023-10-10 17:50:34 -0700

    ASCII

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,26 +2,26 @@
 /**
 * print_sign - Determine the sign of an integer.
 * @n: The integer to be examined.
-* Return:
+* Return: 
 *   - 1 and print '+' if 'n' is greater than zero.
 *   - 0 and print '0' if 'n' is zero.
 *   - (-1) and print '-' if 'n' is less than zero.
 */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return 1;
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return 0;
-    }
-    else
-    {
-        _putchar('-');
-        return -1;
-    }
+if (n > 0)
+{
+_putchar('+');
+return 1;
+}
+else if (n == 0)
+{
+_putchar('0');
+return 0;
+}
+else
+{
+_putchar('-');
+return -1;
+}
 }
