7ad007aa87b4c38b2d4b1ff010480b6e91a55730
AdhamKhaledDev <adhamk109@outlook.com>
2023-10-12 14:55:03 -0700

    Caught_Num_0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* main - Testing the function if it is a positive or a negative
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+	int i;
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
