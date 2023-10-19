aa32b4ba374f81bf61d2c0a9c559700871d38c7d
AdhamKhaledDev <adhamk109@outlook.com>
2023-10-19 15:04:15 -0700

    String_Concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-  for (int i = 0; dest[i] != '\0'; ++i) {}
-  for (int j = 0; src[j] != '\0'; ++j) {
-    dest[i] = src[j];
-    ++i;
-  }
-  dest[i] = '\0';
-  return (dest);
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
