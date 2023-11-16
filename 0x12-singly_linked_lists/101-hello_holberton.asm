618656807b0a5f4b6e0f2bf6894a3598afa4580a
AdhamKhaledDev <adhamk109@gmail.com>
2023-11-16 14:27:12 -0800

    Assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,9 @@
-global main
-extern printf
-
-section .data
-format_string db 'Hello, Holberton\n', 0
-
-section .text
+		  global    main
+		  extern    printf
 main:
-	mov edi, format_string  ; Load the address of the format string
-	xor eax, eax            ; Clear the eax register
-	call printf             ; Invoke the printf function
-	mov eax, 0              ; Set the return value to 0
-	ret                     ; Return from the main function
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
