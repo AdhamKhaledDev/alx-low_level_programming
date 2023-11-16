394eefd9cc97d62b3dd51e0e7ab29e8a60b77a87
AdhamKhaledDev <adhamk109@gmail.com>
2023-11-16 14:25:50 -0800

    Assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,23 +1,13 @@
-section .data
-    hello db 'Hello, Holberton', 0
-
-section .text
-    global _start
-
-_start:
-    ; Write the string to stdout using printf
-    mov rdi, hello
-    mov rax, 0            ; syscall number for sys_write
-    call printf
-    ; Exit the program
-    mov rax, 60           ; syscall number for sys_exit
-    xor rdi, rdi          ; exit code 0
-    syscall
-
-section .text
-    ; External function declarations
-    extern printf
+global main
+extern printf
 
 section .data
-    ; Define the format string for printf
-    fmt db "%s", 0
+format db 'Hello, Holberton\n', 0
+
+section .text
+main:
+	mov edi, format     ; Set the format string
+	xor eax, eax        ; Clear the eax register
+	call printf         ; Call printf function
+	mov eax, 0          ; Set return value to 0
+	ret                 ; Return from the main function
